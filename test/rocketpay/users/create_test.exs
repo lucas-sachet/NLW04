1c678764d49d421d73d6beb942036593863397e0
Lucas Sachet <lucasachet@gmail.com>
2021-03-02 10:19:32 -0300

    readme update

---
+++ test/rocketpay/users/create_test.exs
@@ -0,0 +1,40 @@
+defmodule Rocketpay.Users.CreateTest do
+  use Rocketpay.DataCase, async: true
+
+  alias Rocketpay.User
+  alias Rocketpay.Users.Create
+
+  describe "call/1" do
+    test "when all params are valid, retuns an user" do
+      params = %{
+        name: "Lucas",
+        password: "123465",
+        nickname: "Pew",
+        email: "lucas@pew.com",
+        age: 25
+      }
+
+      {:ok, %User{id: user_id }} = Create.call(params)
+      user = Repo.get(User, user_id)
+
+      assert %User{name: "Lucas", age: 25, id: ^user_id} = user
+    end
+
+    test "when there are invalid params, retuns an user" do
+      params = %{
+        name: "Lucas",
+        nickname: "Pew",
+        email: "lucas@pew.com",
+        age: 15
+      }
+
+      {:error, changeset} = Create.call(params)
+
+      expected_response =%{
+        age: ["must be greater than or equal to 18"],
+        password: ["can't be blank"]
+      }
+      assert errors_on(changeset) == expected_response
+    end
+  end
+end
